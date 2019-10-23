959dd50cbaa35a30e9efdcbef2576ba140bd9b4f
Darren Campbell <darren2802@gmail.com>
2019-10-23 10:59:31 -0600

    Adds iteration 4

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,13 @@
 class Museum
 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons, :revenue
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
+    @revenue = 0
+    @patrons_of_exhibits = Hash.new
   end
 
   def add_exhibit(exhibit)
@@ -17,6 +20,41 @@ class Museum
       recommend << exhibit if patron.interests.any? { |interest| interest == exhibit.name }
     end
     recommend
+  end
+
+  def admit(patron)
+    @patrons << patron
+    recommended_exhibits = recommend_exhibits(patron).sort_by { |reco_exhibit| -reco_exhibit.cost }
+
+    recommended_exhibits.each do |exhibit|
+      if patron.spending_money >= exhibit.cost
+        @revenue += exhibit.cost
+        patron.spending_money -= exhibit.cost
+        if @patrons_of_exhibits[exhibit]
+          @patrons_of_exhibits[exhibit] << patron
+        else
+          @patrons_of_exhibits[exhibit] = [patron]
+        end
+      end
+    end
+
+  end
+
+  def patrons_by_exhibit_interest
+
+    interests_grouped = Hash.new
+
+    @exhibits.each do |exhibit|
+      interests_grouped[exhibit] = []
+      @patrons.each do |patron|
+        interests_grouped[exhibit] = [] << patron if patron.interests.any? { |interest| interest == exhibit.name }
+      end
+    end
+    interests_grouped
+  end
+
+  def patrons_of_exhibits
+    @patrons_of_exhibits
   end
 
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -44,4 +44,110 @@ class TestMuseum < Minitest::Test
     assert_equal [@imax], @dmns.recommend_exhibits(@sally)
   end
 
+  def test_museum_admit_patrons
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("Dead Sea Scrolls")
+    assert_equal [], @dmns.patrons
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+    assert_equal [@bob, @sally], @dmns.patrons
+  end
+
+  def test_museum_patrons_by_exhibit_interest
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+    interests = {
+      @gems_and_minerals => [@bob],
+      @dead_sea_scrolls => [@bob, @sally],
+      @imax => []
+    }
+    assert_equal interests, @dmns.patrons_by_exhibit_interest
+  end
+
+  def test_museum_spending_money_tj
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    @dmns.admit(tj)
+    assert_equal 7, tj.spending_money
+  end
+
+  def test_museum_spending_money_bob
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @bob.spending_money -= 10
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("IMAX")
+    @dmns.admit(@bob)
+    assert_equal 0, @bob.spending_money
+  end
+
+  def test_museum_spending_money_sally
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @sally.add_interest("Dead Sea Scrolls")
+    @sally.add_interest("IMAX")
+    @dmns.admit(@sally)
+    assert_equal 5, @sally.spending_money
+  end
+
+  def test_museum_spending_money_morgan
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Dead Sea Scrolls")
+    morgan.add_interest("Gems and Minerals")
+    @dmns.admit(morgan)
+    assert_equal 5, morgan.spending_money
+  end
+
+  def test_museum_revenue
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    @dmns.admit(tj)
+
+    @bob.spending_money -= 10
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("IMAX")
+    @dmns.admit(@bob)
+
+
+    @sally.add_interest("Dead Sea Scrolls")
+    @sally.add_interest("IMAX")
+    @dmns.admit(@sally)
+
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Dead Sea Scrolls")
+    morgan.add_interest("Gems and Minerals")
+    @dmns.admit(morgan)
+
+    assert_equal 35, @dmns.revenue
+
+  end
+
 end
